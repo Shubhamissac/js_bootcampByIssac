ffba115c435b72cee58e8bcceadfa31193281e43
Shubham Sharma <160347194+Shubhamissac@users.noreply.github.com>
2025-02-24 19:12:56 +0000

    math & num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,38 @@
+// const score  = 100
+
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// const othernumber = 113.3453246
+// console.log(othernumber.toPrecision(4))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))   // for indian number system commas
+
+// console.log(hundreds.toLocaleString())
+
+
+
+// ++++++++++++++++  Maths  +++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(65.939))
+// console.log(Math.ceil(4.2)) // 5
+// console.log(Math.floor(4.2)) //4
+
+
+console.log(Math.random())  // value always comes between 0 & 1
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) /// formula for dice game
